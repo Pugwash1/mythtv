f97ba0ae3d488ca0fbe6b443534b2a0df2fe8bf2
Steve G <RailRodder@sympatico.ca>
2023-03-26 16:38:57 -0400

    my fixes

--- mythtv/programs/mythbackend/scheduler.cpp
+++ mythtv/programs/mythbackend/scheduler.cpp
@@ -3570,9 +3570,8 @@ void Scheduler::PutInactiveSlavesToSleep(void)
     for (auto * enc : qAsConst(*m_tvList))
     {
         if ((!enc->IsLocal()) &&
-            (enc->IsAwake()) &&
-            (!SlavesInUse.contains(enc->GetHostName())) &&
-            (!enc->IsFallingAsleep()))
+            (enc->IsAwake() || enc->IsFallingAsleep() ) &&
+            (!SlavesInUse.contains(enc->GetHostName())))
         {
             QString sleepCommand =
                 gCoreContext->GetSettingOnHost("SleepCommand",
--- mythtv/programs/mythtranscode/transcode.cpp
+++ mythtv/programs/mythtranscode/transcode.cpp
@@ -199,7 +199,7 @@ int Transcode::TranscodeFile(const QString &inputname,
                              bool fifo_info, bool cleanCut,
                              frm_dir_map_t &deleteMap,
                              int AudioTrackNo,
-                             bool passthru)
+                             bool forcefps, bool passthru)
 {
     QDateTime curtime = MythDate::current();
     QDateTime statustime = curtime;
@@ -383,6 +383,13 @@ int Transcode::TranscodeFile(const QString &inputname,
     bool skippedLastFrame = false;
 
     m_kfaTable = new std::vector<struct kfatable_entry>;
+    
+    if (forcefps) {
+        // we want the true fps so we do this since thats all we have
+        if (video_frame_rate > 30) {
+            video_frame_rate = video_frame_rate /2;
+        }
+    }
 
     if (m_avfMode)
     {
@@ -1086,7 +1093,7 @@ int Transcode::TranscodeFile(const QString &inputname,
             std::chrono::milliseconds viddelta = frame.m_timecode - vidTime;
             std::chrono::milliseconds delta = viddelta - auddelta;
             std::chrono::milliseconds absdelta = std::chrono::abs(delta);
-            if (absdelta < 500ms && absdelta >= vidFrameTimeMs)
+            if (absdelta < 500ms && absdelta >= ( vidFrameTimeMs * 2) )
             {
                QString msg = QString("Audio is %1ms %2 video at # %3: "
                                      "auddelta=%4, viddelta=%5")
